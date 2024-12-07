7f29d73a151b29640334d52f66b128ada1342ee1
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-12-07 18:48:05 +0700

    [Fix] Supplier Invoice Detail not showing

--- controller/expenses/supplierInvocieDetailController.js
+++ controller/expenses/supplierInvocieDetailController.js
@@ -3,10 +3,12 @@ const app = express.Router();
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient;
 
-app.get('/all',async (req,res)=>{
+app.get('/all:invoiceno',async (req,res)=>{
     try {
-        const data = await prisma.supplierInvoice.findMany({include: {supplierInvoiceDetail: true, supplierPayment: true} })
-        console.log(data)
+        let param = req.params.invoiceno.replace(':','')
+        console.log("req.params",param)
+        const data = await prisma.supplierInvoiceDetail.findMany({where: { supplierInvoiceId: param } })
+        console.log("req.params",data)
         res.send(data)
     } catch (e) {
         res.status(500).json({ error: e.message })        
@@ -17,16 +19,13 @@ app.post('/create', async (req,res)=>{
     try {
         const getData = req.body
         console.log(getData)
-        const data = await prisma.supplierInvoice.create({data: 
+        const data = await prisma.supplierInvoiceDetail.create({data: 
             { 
                 id: getData.id,
-                invoiceNo: getData.invoiceNo,
-                supplierId: getData.supplierId,
-                date: getData.date,
-                title: getData.title,
-                description: getData.description,
-                userId: getData.userId, 
-                totalAmount: getData.totalAmount        
+                supplierInvoiceId: getData.supplierInvoiceId,
+                productId: getData.productId,
+                purchaseQty: getData.purchaseQty,
+                purchaseUnitPrice: getData.purchaseUnitPrice,    
             }
         })
         res.send(data)
@@ -39,8 +38,7 @@ app.post('/delete', async (req,res)=>{
     try {
         const getData = req.body
         console.log(1234,getData.id)
-        // const data2 = await prisma.supplierInvoiceDetail.delete( { where: { supplierInvoiceId: getData.id} } )
-        const data = await prisma.supplierInvoice.delete( { where: { id: getData.id} } )
+        const data = await prisma.supplierInvoiceDetail.delete( { where: { id: getData.id} } )
         res.send(data)
     } catch (e) {
          res.status(500).json({ error: e.message })   
